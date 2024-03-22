7ef1b8865ec620fec511566f5090f2fc9ee8880a
i.chapaev <i.chapaev@smartway.today>
2024-03-22 21:23:01 +0300

    fix

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ git clone git@github.com:ch1llay/ViolationsDetection.git
 apt  install docker.io
 apt  install docker-compose
 
-./ConfigurationSeconSecon/updateWithDB.sh
+./ConfigurationSecon/updateWithDB.sh
 
